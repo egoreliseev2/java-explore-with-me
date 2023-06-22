f43606f25a5079d2bbef7815cd27c2b5a3273ec6
Egor Eliseev <egoreliseev2@yandex.ru>
2023-06-22 07:24:27 +0300

    v1

--- ewm-stat/stat-server/src/main/java/ru/practicum/ewm/model/ViewStat.java
+++ ewm-stat/stat-server/src/main/java/ru/practicum/ewm/model/ViewStat.java
@@ -1,17 +1,14 @@
 package ru.practicum.ewm.model;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.experimental.FieldDefaults;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ViewStat {
-    String uri;
-    String app;
-    Long hits;
+    private String uri;
+    private String app;
+    private Long hits;
 }
