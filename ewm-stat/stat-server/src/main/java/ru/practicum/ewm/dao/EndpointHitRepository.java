70e5d2ef34cbcaa67a99f9913efc3595d83c6704
Egor Eliseev <egoreliseev2@yandex.ru>
2023-06-22 07:37:30 +0300

    v1

--- ewm-stat/stat-server/src/main/java/ru/practicum/ewm/dao/EndpointHitRepository.java
+++ ewm-stat/stat-server/src/main/java/ru/practicum/ewm/dao/EndpointHitRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Repository
 public interface EndpointHitRepository extends JpaRepository<EndpointHit, Long> {
 
-    @Query(value = "SELECT new ru.practicum.model.ViewStat(e.uri, e.app, COUNT(DISTINCT e.ip)) " +
+    @Query(value = "SELECT new ru.practicum.ewm.model.ViewStat(e.uri, e.app, COUNT(DISTINCT e.ip)) " +
             "FROM EndpointHit e " +
             "WHERE e.timestamp BETWEEN :start AND :end " +
             "AND e.uri IN :uris " +
@@ -20,7 +20,7 @@ public interface EndpointHitRepository extends JpaRepository<EndpointHit, Long> 
             "ORDER BY COUNT(e.ip) DESC")
     List<ViewStat> getViewStatsUnique(LocalDateTime start, LocalDateTime end);
 
-    @Query(value = "SELECT NEW ru.practicum.model.ViewStat(e.uri, e.app, COUNT(DISTINCT e.ip)) " +
+    @Query(value = "SELECT NEW ru.practicum.ewm.model.ViewStat(e.uri, e.app, COUNT(DISTINCT e.ip)) " +
             "FROM EndpointHit e " +
             "WHERE e.uri IN :uris " +
             "AND e.timestamp BETWEEN :start AND :end " +
@@ -28,14 +28,14 @@ public interface EndpointHitRepository extends JpaRepository<EndpointHit, Long> 
             "ORDER BY COUNT(DISTINCT e.ip) DESC")
     List<ViewStat> getViewStatsByUrisAndUnique(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query("SELECT new ru.practicum.model.ViewStat(e.uri, e.app, COUNT(e.ip)) " +
+    @Query("SELECT new ru.practicum.ewm.model.ViewStat(e.uri, e.app, COUNT(e.ip)) " +
             "FROM EndpointHit e " +
             "WHERE e.timestamp BETWEEN :start AND :end " +
             "GROUP BY e.app, e.uri " +
             "ORDER BY COUNT(e.ip) DESC")
     List<ViewStat> getViewStats(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT new ru.practicum.model.ViewStat(e.uri, e.app, COUNT(e.ip)) " +
+    @Query("SELECT new ru.practicum.ewm.model.ViewStat(e.uri, e.app, COUNT(e.ip)) " +
             "FROM EndpointHit e " +
             "WHERE e.timestamp BETWEEN :start AND :end " +
             "AND e.uri IN :uris " +
